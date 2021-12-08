83032c8db33281f1229d9d4c132dd564c6bc2601
Jan Bína <binajohny@gmail.com>
2021-12-08 21:20:46 +0100

    Day 8 simplification

--- src/main/kotlin/solutions/Day08.kt
+++ src/main/kotlin/solutions/Day08.kt
@@ -30,64 +30,25 @@ class Day08(
     }
 
     private fun Entry.resolveOutput(): Int {
-        val mapping = signalPatterns.determineMapping()
+        val mapping = signalPatterns.createMapping()
         return outputValue.map {
-            it.toDigit(mapping)
+            mapping[it] ?: error("No mapping for $it")
         }.joinToString("").toInt()
     }
 
-    private fun Set<Char>.toDigit(mapping: Map<Char, Char>): Int {
-        return when (size) {
-            2 -> 1
-            3 -> 7
-            4 -> 4
-            7 -> 8
-            5 -> {
-                val mapped = map { mapping[it] }
-                when {
-                    'e' in mapped -> 2
-                    'b' in mapped -> 5
-                    else -> 3
-                }
-            }
-            6 -> {
-                val mapped = map { mapping[it] }
-                when {
-                    'd' !in mapped -> 0
-                    'c' !in mapped -> 6
-                    else -> 9
-                }
-            }
-            else -> error("Invalid size")
-        }
-    }
-
-    private fun List<Set<Char>>.determineMapping(): Map<Char, Char> {
-        val mapping = mutableMapOf<Char, Char>()
-
-        val d1 = first { it.size == 2 }
-        val d7 = first { it.size == 3 }
-
-        mapping['a'] = (d7 - d1).single()
-
-        val d6 = first { it.size == 6 && it.intersect(d1).size == 1 }
-
-        mapping['c'] = (d1 - d6).single()
-        mapping['f'] = d1.intersect(d6).single()
-
-        val d3 = first { it.size == 5 && it.intersect(d1).size == 2 }
-        val segDG = d3 - d7
-        val d0 = first { it.size == 6 && it.intersect(segDG).size != 2 }
-
-        mapping['d'] = (segDG - d0).single()
-        mapping['g'] = segDG.intersect(d0).single()
-
-        val d5 = first { it.size == 5 && mapping['c'] !in it }
-
-        mapping['b'] = (d5 - d3).single()
-        mapping['e'] = (d0 - d5 - d1).single()
-
-        return mapping.entries.associateBy({ it.value }) { it.key }
+    private fun List<Set<Char>>.createMapping(): Map<Set<Char>, Int> {
+        return mutableMapOf<Int, Set<Char>>().apply {
+            this[1] = first { it.size == 2 }
+            this[4] = first { it.size == 4 }
+            this[7] = first { it.size == 3 }
+            this[8] = first { it.size == 7 }
+            this[9] = first { it.size == 6 && it.containsAll(this[4]!!) }
+            this[6] = first { it.size == 6 && !it.containsAll(this[7]!!) }
+            this[0] = first { it.size == 6 && it != this[6] && it != this[9] }
+            this[3] = first { it.size == 5 && it.containsAll(this[1]!!) }
+            this[2] = first { it.size == 5 && it.intersect(this[4]!!).size == 2 }
+            this[5] = first { it.size == 5 && it != this[3] && it != this[2] }
+        }.entries.associateBy({ it.value }) { it.key }
     }
 
     private data class Entry(
